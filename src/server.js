98cdfbe12146e122ebe9af68e6ce13fefe9898e1
Mohini Ruhela <vitthalruhela123@gmail.com>
2026-08-31 18:38:21 +0530

    setting up server.js , from where our actual Http server will starts

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import "dotenv/config";
+
+import app from "./app.js";
+
+
+const PORT = process.env.PORT || 5000;
+
+
+const server = app.listen(PORT, () => {
+    console.log(
+        `Server running on port ${PORT}`
+    );
+});
+
+
+export default server;
